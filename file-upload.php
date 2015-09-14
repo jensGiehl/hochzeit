eeacad24a6662cebff3fe431d246ed05443310e7
Jens Giehl <->
2015-09-14 21:45:36 +0200

    !NOT WORKING! Tried to refactor upload

--- file-upload.php
+++ file-upload.php
@@ -1,19 +1,18 @@
 <?php
 include_once 'auth.php';
+include_once 'config.php';
 
 doLoginCheck();
 
 $ds = DIRECTORY_SEPARATOR;
 
-$storeFolder = 'uploads';
+$storeFolder = $config['upload']['folder'];
 
-if (!empty($_FILES)) {
-
-    $tempFile = $_FILES['file']['tmp_name'];
-
-    $targetPath = dirname(__FILE__) . $ds . $storeFolder . $ds;
-
-    $targetFile = $targetPath . $_FILES['file']['name'];
-
-    move_uploaded_file($tempFile, $targetFile);
+print_r(array_values($_FILES));
+foreach ($_FILES["file"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["file"]["tmp_name"][$key];
+        $name = uniqid() . "_" . $_FILES["file"]["name"][$key];
+        move_uploaded_file($tmp_name, dirname(__FILE__) . $ds . $storeFolder . $ds . $name);
+    }
 }
