efe87cb393e166cc7ca7b6347e2d6926950dcdbb
Thomas Ohler <->
2015-09-21 23:33:46 +0200

    !!NOT Working correct! Implementation of the Slider in the header

--- galerie.php
+++ galerie.php
@@ -24,18 +24,25 @@ doLoginCheck();
 <div class="bgbox" id="bg">
     <div id="container">
         <header>
-            <p id="p1">Wir</p>
+            <div id="bg-image">
+				<img class="bottom" src="./images/ju1.jpg" />
+				<img class="top" src="./images/ju2.jpg" />
+				<img class="top" src="./images/ju3.jpg" />
+				<img class="bottom" src="./images/ju1.jpg" />
+					<!--<div id="htext">
+						<p id="p1">Wir</p>
+						<p id="p2"> heiraten</p>
+					</div>-->
+				<nav id="nav">
+					<ul id="navigation">
+						<li><a href="start.php" title="Home">Home</a></li>
+						<li><a href="#" title="Über uns">Über Uns</a></li>
+						<li><a href="#" title="Kontakt">Kontakt</a></li>
+						<li><a href="galerie.php" title="Galerie">Galerie</a></li>
 
-            <p id="p2"> heiraten</p>
-            <nav id="nav">
-                <ul id="navigation">
-                    <li><a href="start.php" title="Home">Home</a></li>
-                    <li><a href="#" title="Über uns">Über Uns</a></li>
-                    <li><a href="#" title="Kontakt">Kontakt</a></li>
-                    <li><a href="galerie.php" title="Galerie">Galerie</a></li>
-
-                </ul>
-            </nav>
+					</ul>
+				</nav>
+			</div>
         </header>
 
         <main>
