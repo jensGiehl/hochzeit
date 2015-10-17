f329bce8db769eeba74e2d6ac0a3013514cb970b
Jens Giehl <->
2015-10-17 21:39:50 +0200

    Added footer and header for page

---
+++ pageHeader.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ulrike + Jens</title>
+    <meta name="Language" content="de"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <style type="text/css">@import "./css/design.css";</style>
+</head>
+<body>
