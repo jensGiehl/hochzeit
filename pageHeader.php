d3928ef0ee6891044f30a0ad66ff15f8750d5cf8
Jens Giehl <->
2015-10-17 22:07:45 +0200

    Added language and encoding

--- pageHeader.php
+++ pageHeader.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head lang="de">
     <title>Ulrike + Jens</title>
     <meta name="Language" content="de"/>
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <style type="text/css">@import "./css/design.css";</style>
