1797e9035f0c23223877858aec66fcd05868c5c6
Jens Giehl <->
2015-10-27 21:06:26 +0100

    Added bootstrap grid

--- start.php
+++ start.php
@@ -8,9 +8,27 @@ include_once 'pageHeader.php';
 <div id="carousel-header">
 	<?php include 'carousel.php'?>
 </div>
-<br/><br/>
-<div id="upload-images">
-	<?php include 'upload.php'?>
+<div class="container-fluid">
+
+	<div class="row">
+		<div class="col-md-12">
+			<br/><br/>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-1">
+		</div>
+
+		<div class="col-md-10">
+			<div id="upload-images">
+				<?php include 'upload.php'?>
+			</div>
+		</div>
+
+		<div class="col-md-1">
+		</div>
+	</div>
 </div>
 <?php
 include_once 'pageFooter.php';
