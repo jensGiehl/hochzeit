ab0136ff086c6df7cf4e2f1e6ab2d745f38ab937
Jens Giehl <->
2015-10-27 19:26:58 +0100

    Added dropzone css

--- pageHeader.php
+++ pageHeader.php
@@ -7,8 +7,8 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+	<link rel="stylesheet" href="css/dropzone.css">
 	<link rel="stylesheet" href="css/styles.css" />
-	<!-- <style type="text/css">@import "./css/design.css";</style>  -->
 	<script src="js/dropzone.js"></script>
 </head>
 <body>
