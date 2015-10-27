3a697498a7878fa989ebc1cef794d2bfe08f71b0
Jens Giehl <->
2015-10-27 19:43:15 +0100

    Added 'name' on login page

--- index.php
+++ index.php
@@ -26,16 +26,23 @@ include_once 'pageHeader.php';
     <?php
     if ($error) {
         ?>
-        <p class="bg-danger">Leider haben Sie das falsche Passwort eingegeben!</p>
+        <p class="bg-danger">Leider hast du das falsche Passwort eingegeben!</p>
         <?php
     }
     ?>
     <form method="post" action="index.php">
-        <div class="form-group">
+        <div class="form-group text-left">
+        	<label for="username">Dein Name?</label>
+        	<input 	type="text" class="form-control" id="username" name="username"
+        			value="<?=$_POST['username']?>"
+        			placeholder="Dein Name" required <?php if ($error == false) { echo "autofocus"; } ?>/>
+
+			<br/><br/>
+
             <label for="sitepassword">Zugangspasswort</label>
             <input type="password" class="form-control" id="sitepassword" name="sitepassword"
                    placeholder="Zugangspassword"
-                   required autofocus>
+                   required <?php if ($error) { echo "autofocus"; } ?>>
         </div>
         <button type="submit" class="btn btn-primary" name="submit">Seite betreten</button>
     </form>
