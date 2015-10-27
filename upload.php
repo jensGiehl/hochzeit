1992a5d6aff31e85fc19cd2d3197c401fa9b8efc
Jens Giehl <->
2015-10-27 19:27:28 +0100

    Added german message to dropzone

--- upload.php
+++ upload.php
@@ -4,6 +4,6 @@ include_once 'auth.php';
 doLoginCheck();
 
 ?>
-<form action="file-upload.php"
-      class="dropzone"
-      id="dz-gallery"></form>
+<form action="file-upload.php" class="dropzone" id="dz-gallery">
+	<div class="dz-message" data-dz-message><span>Bilder hier reinziehen oder klicken & ausw&auml;hlen</span></div>
+</form>
