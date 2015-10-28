a7590c2ecae9cb9ec354ce5b3ffebdc54183f9a5
Jens Giehl <->
2015-10-28 11:13:05 +0100

    Changed font

--- upload.php
+++ upload.php
@@ -5,5 +5,5 @@ doLoginCheck();
 
 ?>
 <form action="file-upload.php" class="dropzone" id="dz-gallery">
-	<div class="dz-message" data-dz-message><span>Bilder hier reinziehen oder klicken &amp; ausw&auml;hlen</span></div>
+	<div class="dz-message" data-dz-message><span class="font-text-small">Bilder hier reinziehen oder klicken &amp; ausw&auml;hlen</span></div>
 </form>
