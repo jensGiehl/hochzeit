9d822402aad3e3d2967efaeb7ee2c574774de607
Jens Giehl <->
2015-10-28 11:02:22 +0100

    Added flying heart animation

--- start.php
+++ start.php
@@ -24,5 +24,9 @@ include_once 'pageHeader.php';
 <div class="wedding_pair">
 	<img src="images/wedding_kissing.png" />
 </div>
+
+<div id="heart" class="animated">
+	<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABUklEQVQ4T6WT300CQRjEZ/YCMeGIdCBWACaezyf4Lh2IHUgFYgVoBWIFXgFK8FVIhA4sAcIRDXg3Zgl3IWriofu4+83v+zdL/PMw0cuvlt6Zq0SGZUQau/3BaJMd+l4VDitOrNcdLcfsjyb2fQUIa15TQAcECQQAzwQEbrQ4t+9zJ3cP0E+TSRMCLbc36HJ2cuhDDCi1BTaM1JExgQ22EJuEwOlPnYrxMWc1r0+gW4iXwdzkX0CUs45F0hPD+pEQ6QBGPmg6WcWrCm0rs7o3YQT/TwBoyrDudQXurifa2KYCQHd886vlDyc34hqyDcCJFvvpGkHebiOG4pbbG16nRrJeyA7Rnfs4aKZGSjJngUi4Kvae24kmrSCFWMsaBiD2NlsSNAXVKD4M+5v33wCr/frVUmjyFyQuV8HSTSFethP//wpIAuyGIuRKXz9WZkCWrXwCl/GJLYrwAjcAAAAASUVORK5CYII="/>
+</div>
 <?php
 include_once 'pageFooter.php';
