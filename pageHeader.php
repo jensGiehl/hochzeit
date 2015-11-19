cfcf8e6cb28de96dc8f2f3ff53be75b4fd89453d
Jens Giehl <->
2015-11-19 21:51:36 +0100

    Added viewpoint for mobile devices

--- pageHeader.php
+++ pageHeader.php
@@ -5,6 +5,7 @@
 	<meta name="Language" content="de" />
 	<meta charset="utf-8" />
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 	<link rel="stylesheet" href="css/dropzone.css">
