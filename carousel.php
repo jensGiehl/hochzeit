76837f546f4202ed9feb260d37d27b31a704e915
Jens Giehl <->
2015-11-19 21:51:21 +0100

    Images have now always 100% width

--- carousel.php
+++ carousel.php
@@ -10,16 +10,16 @@ include_once 'pageHeader.php';
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="images/ju1.jpg" />
+      <img src="images/ju1.jpg" width="100%"/>
     </div>
     <div class="item">
-      <img src="images/ju2.jpg" />
+      <img src="images/ju2.jpg" width="100%"/>
     </div>
     <div class="item">
-      <img src="images/ju4.jpg" />
+      <img src="images/ju4.jpg" width="100%"/>
     </div>
     <div class="item">
-      <img src="images/ju3.jpg" />
+      <img src="images/ju3.jpg" width="100%"/>
     </div>
   </div>
 </div>
